4ab5c92748959068daddccb0aa04935f3319841e
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-03-09 22:51:22 +0000

    FireCanScaleDown

--- Assets/Scripts/MarekScript/Fire.cs
+++ Assets/Scripts/MarekScript/Fire.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    public ParticleSystem fire;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fire = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        fire.transform.localScale -= new Vector3(0.001f, 0.001f, 0.001f);
+        if (fire.transform.localScale == Vector3.zero)
+        {
+            fire.gameObject.SetActive(false);
+        }
         
     }
 }
