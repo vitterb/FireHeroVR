3c8cefff95b667ed04e24cefef773eee27d4e51c
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-03-11 17:11:52 +0000

    AddFireAndExtinguisherCode

--- Assets/Scripts/MarekScript/Fire.cs
+++ Assets/Scripts/MarekScript/Fire.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    public float fireGrow = 0.5f;
+    public float waitTime = 5f;
+    private Vector3 startingScale;
     public ParticleSystem Marekfire;
-
-    public ParticleSystem extinguisher;
+    public ParticleSystem Mareksmoke;
     // Start is called before the first frame update
     void Start()
     {
-        extinguisher = GetComponent<ParticleSystem>();
         Marekfire = GetComponent<ParticleSystem>();
+        Mareksmoke = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         /*Marekfire.transform.localScale -= new Vector3(0.001f, 0.001f, 0.001f);*/
-        if (Marekfire.transform.localScale.magnitude < 0.001f)
+        if (Marekfire.transform.localScale.magnitude < 0.01f && Mareksmoke.transform.localScale.magnitude < 0.01f)
         {
             Marekfire.gameObject.SetActive(false);
         }
@@ -45,10 +47,21 @@ public class Fire : MonoBehaviour
     }*/
     private void OnParticleCollision(GameObject other)
     {
-        if (other.gameObject.CompareTag("MarekFire"))
+        if (other.gameObject.CompareTag("MarekExt"))
         {
-            Marekfire.gameObject.SetActive(false);
+            Marekfire.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            Mareksmoke.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+        }
+    }
+
+    private IEnumerator WaitingForChange()
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (transform.localScale == startingScale)
+        {
+            
         }
     }
     
+
 }
---
+++ Assets/Scripts/MarekScript/FireSpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawn : MonoBehaviour
+{
+    public ParticleSystem FirePrefab;
+    public Transform[] spawnPoints;
+
+    public float numberOfFires;
+    public int spawnDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFires());
+    }
+
+    // Update is called once per frame
+    void SpawnEnemy()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[spawnIndex];
+        ParticleSystem fire = Instantiate(FirePrefab, spawnPoint.position, spawnPoint.rotation);
+     
+    }
+    IEnumerator SpawnFires()
+    {
+        for (int i = 0; i < numberOfFires; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
