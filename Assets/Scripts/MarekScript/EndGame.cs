48e9b114a01973a2f692ae83a473b3bef85af949
Aang121 <123459150+Aang1214@users.noreply.github.com>
2023-04-22 00:21:49 +0100

    add

--- Assets/Scripts/MarekScript/EndGame.cs
+++ Assets/Scripts/MarekScript/EndGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
     public GameObject HealthRestart;
+
+    public Interact goal;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,4 +27,6 @@ public class EndGame : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+   
 }
--- Assets/Scripts/MarekScript/Fuel.cs
+++ Assets/Scripts/MarekScript/Fuel.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Fuel : MonoBehaviour
 {
     public GameObject fuel;
-
     public ParticleSystem Ext;
+    public XRController controller;
     // Start is called before the first frame update
     void Start()
     {
+        controller = GameObject.Find("LeftHand Controller").GetComponent<XRController>();
+        Ext.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        fuel.transform.localScale -= new Vector3(0.0f, 0.0001f, 0.0f);
         if (fuel.transform.localScale.y < 0.1f)
         {
             fuel.SetActive(false);
             Ext.gameObject.SetActive(false);
+        }
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        {
+            fuel.transform.localScale -= new Vector3(0.0f, 0.0001f, 0.0f);
+            Ext.gameObject.SetActive(true);
         }
     }
 }
