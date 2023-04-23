64c33b3b8e1c0ab28b9d490dc75e802079746251
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-04-23 19:18:23 +0100

    Made a beta build

---
+++ Assets/Scripts/bviScripts/timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    private int counter = 150;
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter -= 1;
+        if (counter <= 0)
+            SceneManager.LoadScene("betascene");
+    }
+}
