1f75a97c21f896fa8f2d394e6a5e6b974269a374
Aang1214 <martinak.m@email.cz>
2023-04-25 16:25:49 +0100

    AddButtonForExting

--- Assets/Scripts/MarekScript/Fuel.cs
+++ Assets/Scripts/MarekScript/Fuel.cs
@@ -11,37 +11,44 @@ public class Fuel : MonoBehaviour
     public ParticleSystem Ext;
     public InputActionProperty ybutton;
     public bool full;
+    public bool ok;
     // Start is called before the first frame update
     void Start()
     {
+        
         full = true;
-        Ext.gameObject.SetActive(false);
+        Ext.gameObject.SetActive(true);
+        fuel.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ext.gameObject.SetActive(false);
-        if (ybutton.action.WasPressedThisFrame() && full)
+        ok = true;
+        if (full && ok)
         {
             Ext.gameObject.SetActive(true);
-            fuel.transform.localScale -= new Vector3(0.0f, 0.0001f, 0.0f);
-            if (fuel.transform.localScale.y < 0.1f)
-            {
-                full = false;
-            }
+            fuel.transform.localScale -= new Vector3(0.0f, 0.0f, 0.001f);
         }
-        if (!full)
+        if (ybutton.action.IsPressed() && full)
         {
+            ok = false;
+            Ext.gameObject.SetActive(false);
+            fuel.transform.localScale -= new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        if (fuel.transform.localScale.z < 0.1f)
+        {
+            full = false;
             fuel.SetActive(false);
             Ext.gameObject.SetActive(false);
         }
-        /*fuel.transform.localScale -= new Vector3(0.0f, 0.0001f, 0.0f);
         
+        /*fuel.transform.localScale -= new Vector3(0.0f, 0.0001f, 0.0f);
+
         if (fuel.transform.localScale.y < 0.1f)
         {
             fuel.SetActive(false);
             Ext.gameObject.SetActive(false);
-        }*/
-    }
+    }*/
+}
 }
